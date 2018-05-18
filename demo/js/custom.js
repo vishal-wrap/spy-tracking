bb62191924c98034c9f707653cdd61dd264a94cc
Vishal Bhatt <bvishal157@gmail.com>
2018-05-18 12:35:55 +0530

    'spy'

---
+++ demo/js/custom.js
@@ -0,0 +1,63 @@
+/*
+Template Name: Wrapkit Ui Kit
+Author: Themedesigner
+Email: niravjoshi87@gmail.com
+File: js
+*/
+$(function() {
+    "use strict";
+
+    // ============================================================== 
+    //This is for preloader
+    // ============================================================== 
+
+    $(function() {
+        $(".preloader").fadeOut();
+    });
+
+    // ============================================================== 
+    // Resize all elements
+    // ============================================================== 
+
+    $("body").trigger("resize");
+
+    // ============================================================== 
+    // Animation initialized
+    // ============================================================== 
+
+    AOS.init();
+
+    // ============================================================== 
+    // testimonial
+    // ============================================================== 
+
+    $('.testi3').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: false,
+        dots: true,
+        autoplay: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            1170: {
+                items: 3
+            }
+        }
+    });
+
+    // ============================================================== 
+    // show more less
+    // ============================================================== 
+
+    $('#show_more').on('click', function(e) {
+        $('.features-table').addClass('h-auto');
+    });
+    $('#show_less').on('click', function(e) {
+        $('.features-table').removeClass('h-auto');
+    });
+
+});
